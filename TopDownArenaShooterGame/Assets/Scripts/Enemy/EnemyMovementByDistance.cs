48013247622ea5902719f2558951916ae473e726
NurullahBaser <salvorex006@gmail.com>
2023-01-31 18:14:58 +0300

    new mob movement added

---
+++ TopDownArenaShooterGame/Assets/Scripts/Enemy/EnemyMovementByDistance.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyMovementByDistance : MonoBehaviour
+    {
+        
+        [SerializeField] private Transform target;
+        [SerializeField] private float speed;
+        private Rigidbody2D _rigidbody2D;
+        private bool forward = true;
+        [SerializeField] private float minD;
+        [SerializeField] private float maxD;
+        
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+        
+        private void Update()
+        {
+            Vector2 direction = (target.position - transform.position).normalized;
+            float distance = Vector2.Distance(transform.position, target.transform.position);
+           
+            if (distance <= minD) direction = (transform.position - target.position).normalized;
+            else if (distance >= maxD) direction = (target.position -transform.position).normalized;
+            else direction *= 0;
+
+            _rigidbody2D.velocity = (direction)*speed;
+        }
+    }
+}

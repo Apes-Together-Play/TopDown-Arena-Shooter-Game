4bea4be1e1aafc1336e67e15167d3bc0ac54b969
NurullahBaser <salvorex006@gmail.com>
2023-02-02 13:28:36 +0300

    new enemy added, small enemy changes,
    Duvarlara wall tagı eklendi.

---
+++ TopDownArenaShooterGame/Assets/Scripts/Enemy/Movement/RandomMovement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Enemy.Movement
+{
+    public class RandomMovement : Movement
+    {
+        private Vector2 _direction;
+        
+        protected override void Move()
+        {
+            Rb2D.velocity = _direction * speed;
+        }
+        
+        private new void Start()
+        {
+            base.Start();
+            ChangeDirection();
+        }
+
+        private void ChangeDirection()
+        {
+            _direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            _direction.Normalize();
+        }
+
+        private void OnTriggerStay2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Wall")) ChangeDirection();
+            if(col.gameObject.CompareTag("Enemy")) ChangeDirection();
+            
+        }
+    }
+}

7c0793bb60b90851fb6c9a7df3b28977fe898960
tahirakdeniz <muhammedtahirakdeniz@gmail.com>
2023-03-07 18:23:51 +0300

    character selection done
    buttons added and ui designed
    
    Co-Authored-By: Nurullah Başer <123994929+NurullahBaser@users.noreply.github.com>
    Co-Authored-By: IsmailUlasUnal <123994930+IsmailUlasUnal@users.noreply.github.com>

---
+++ TopDownArenaShooterGame/Assets/Scripts/SceneController/CharacterSelection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Stats;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SceneController
+{
+    public class CharacterSelection : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI textMeshProUGUI;
+        [SerializeField] private Button button;
+        
+        public void Load(StatsUpgrade statsUpgrade)
+        {
+           // TODO edit textMesh pro add some grid
+            textMeshProUGUI.text = statsUpgrade.name + "\n" + 
+                                   statsUpgrade.Description + "\n" ;
+            foreach (var stat in  statsUpgrade.upgradeToApply)
+            {
+                var value = stat.value;
+                var type = stat.statType.ToString();
+                textMeshProUGUI.text += stat.value > 0 ? $"+{value} {type}\n" : $"-{value} {type}\n";
+            }
+
+            if (!button.gameObject.activeInHierarchy)
+            {
+                button.gameObject.SetActive(true);
+            }
+        }
+    }
+}

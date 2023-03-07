35d8d887baba31b16ba93038903b34cb5c43a701
tahirakdeniz <muhammedtahirakdeniz@gmail.com>
2023-03-07 21:16:43 +0300

    player movement improved
    diagonal movement speed problem fixed
    
    Co-Authored-By: IsmailUlasUnal <123994930+IsmailUlasUnal@users.noreply.github.com>
    Co-Authored-By: Nurullah Başer <123994929+NurullahBaser@users.noreply.github.com>

--- TopDownArenaShooterGame/Assets/Scripts/Player/PlayerMovement.cs
+++ TopDownArenaShooterGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,36 +9,20 @@ namespace Player
     {
         private readonly Rigidbody2D rb2d;
         private float speed;
+        public void SetSpeed(float speed) => this.speed = speed;
 
-        public void SetSpeed(float speed)
-        {
-            Debug.Log(speed);
-            this.speed = speed;
-            
-        }
-
-        public void SetSpeedByMultiply(float increaseConstant)
-        {
-            speed *= increaseConstant;
-        }
-
-        public float GetSpeed()
-        {
-            return speed;
-        }
-        
+        public void SetSpeedByMultiply(float increaseConstant) => speed *= increaseConstant;
 
         public PlayerMovement(Rigidbody2D rb2d)
         {
             this.rb2d = rb2d;
         }
-        
+
         public void Move()
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
-
-            var movement = new Vector2(horizontal, vertical);
+            var movement = new Vector2(horizontal, vertical).normalized;
             rb2d.velocity = movement * speed;
         }
     }

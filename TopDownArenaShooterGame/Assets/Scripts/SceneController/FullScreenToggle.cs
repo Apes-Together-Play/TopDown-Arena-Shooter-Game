75b6459daf691c188e1d70e4aa5657776ae1bf7a
tahirakdeniz <muhammedtahirakdeniz@gmail.com>
2023-03-09 15:44:32 +0300

    Loading Scene Settings UI  Created
    I think it is completed buy please check them

---
+++ TopDownArenaShooterGame/Assets/Scripts/SceneController/FullScreenToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SceneController
+{
+    public class FullScreenToggle : MonoBehaviour
+    {
+        [SerializeField] private Sprite minimize;
+        [SerializeField] private Sprite maximize;
+        [SerializeField] private Image image;
+
+        private void Start()
+        {
+            image.sprite = Screen.fullScreen ? minimize : maximize;
+        }
+
+        public void Toggle()
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+            image.sprite = Screen.fullScreen ? minimize : maximize;
+        }
+    }
+}
